d59fc7426805e46f91867c6d9ddc001ba619c442
Nathan Meyers <nathan@runnable.com>
2017-02-07 15:26:39 -0800

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.get('/git', (req, res) => {
   res.send(gitInfo())
 })
 
+app.get('/', (req, res) => {
+  res.redirect(req.protocol + '://subdomain.' + req.get('host') + req.originalUrl;
+})
+
 app.listen(port, () => {
   var hostName = 'http://' + host + ':' + port
   console.log('Application running at: ' + hostName)
